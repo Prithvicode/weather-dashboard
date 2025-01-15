3c47aff396eb8eedf7ac2c19582cbf996a9be9ed
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2025-01-15 14:30:18 +0545

    feat-dashboard created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +27,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         
+         <SidebarProvider>
+          
+       
         {children}
+        </SidebarProvider>
       </body>
     </html>
   );
